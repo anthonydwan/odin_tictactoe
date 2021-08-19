ab557b036d8a5c68cc5f698be28d96feda16e93d
anthony <anthonydwan@outlook.com>
2021-08-19 19:16:31 +1000

    starting off with some modules

--- script.js
+++ script.js
@@ -1,33 +1,94 @@
-const gameBoard = (function(){
-    const board = Array(9)
+const player = (sign) => {
+    // 
+}
 
+const gameBoard = (() => {
+    // create the board logic in the game
+    let board = {}
+    for (let i = 0; i < 9; i++) {
+        board[i] = "_"
+    }
 
-    // reveal public pointers to 
-    // private functions and properties
+    let getLegalMoves = () => {
+        let legalCells = []
+        for (cell in board) {
+            if (board[cell] === "_") {
+                legalCells.push(parseInt(cell))
+            }
+        }
+        return legalCells
+    }
+
+    let setMove = (cell, sign) => {
+        board[cell] = sign
+    }
+
+    return {
+        // board should be removed later
+        board,
+        getLegalMoves,
+        setMove
+    }
 
 })();
 
-const displayControl = (function(){
-    const container = document.querySelector("#container")
-    function createBoard(){
-        for(let i =0;i< 9; i++){
+const displayControl = (() => {
+    const _container = document.querySelector("#container")
+
+    const createBoard = () => {
+        for (let i = 0; i < 9; i++) {
             const box = document.createElement("div")
-            box.classList.add("#box")
-            container.appendChild(box)
+            box.classList.add("box")
+            box.id = `cell${i}`
+            _container.appendChild(box)
         }
     }
-    return createBoard
+    createBoard()
+
+    const displayMove = () => {
+        // once you get a move from the gameControl -> 
+        // gameBoard -> display
+        // display it 
+    }
+    return {};
 })();
 
 
-// const gameControl
+const gameControl = (() => {
+    let playerCounter = 0
+
+    const turnControl = () => {
+        playerCounter ++
+        if (playerCounter ===1){
+            return "O"
+        } else if (playerCounter === 2){
+            playerCounter = 0
+            return "X"
+        }
+    };
+
+    const checkLegalMove = function (cellNumber) {
+        return (gameBoard.getLegalMoves().includes(cellNumber) ? true : false) 
+    };
 
 
+    const registerMove = function () {
+        const cellNumber = parseInt(this.id.slice(4))
+        if (checkLegalMove(cellNumber)) {
+            const sign = turnControl()
+            gameBoard.setMove(cellNumber, sign)
+        }
+    }
 
-// const player = (sign) => {
-//     sign
-//     const playMove
-//     return {}
-// }
+    const registerClick = () => {
+        const boxes = document.querySelectorAll(".box")
+        for (box of boxes) {
+            box.addEventListener('click', registerMove)
+        }
+    }
 
-const createDisplay = displayControl();
+    registerClick()
+
+})();
+
+
