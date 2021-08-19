6af07d4ac92abb9aeebd0c4c22551d065e1ff04b
anthony <anthonydwan@outlook.com>
2021-08-19 21:31:25 +1000

    various core mechanics done

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const player = (sign) => {
-    // 
+const player = () => {
+    let movesMade = []
+
+    const registerMove = (cell) =>{
+        movesMade.push(cell)
+    }
+
+    return {
+        movesMade,
+        registerMove
+    }
 }
 
 const gameBoard = (() => {
     // create the board logic in the game
-    let board = {}
-    for (let i = 0; i < 9; i++) {
-        board[i] = "_"
+
+    const createBoard = () => {
+        let board = {}
+        for (let i = 0; i < 9; i++) {
+            board[i] = "_"
+        }
+        return board
     }
 
-    let getLegalMoves = () => {
+    const getLegalMoves = () => {
         let legalCells = []
         for (cell in board) {
             if (board[cell] === "_") {
@@ -19,17 +32,17 @@ const gameBoard = (() => {
         return legalCells
     }
 
-    let setMove = (cell, sign) => {
+    const setMove = (cell, sign) => {
         board[cell] = sign
     }
 
+    let board = createBoard()
+
     return {
-        // board should be removed later
         board,
         getLegalMoves,
         setMove
     }
-
 })();
 
 const displayControl = (() => {
@@ -43,40 +56,77 @@ const displayControl = (() => {
             _container.appendChild(box)
         }
     }
+
+    const getCellDiv = (cellNumber) =>{
+        return document.querySelector(`#cell${cellNumber}`)
+    }
+
+    const displayMove = (playerIndex, cellNumber) => {
+        const div = getCellDiv(cellNumber)
+        div.classList.add(`player${playerIndex}`)
+    }
+
+    const resetBoard = () =>{
+        for (let i = 0; i < 9; i++) {
+            const div = getCellDiv(i)
+            div.classList.remove("player0", "player1")
+        }
+    }
+
     createBoard()
 
-    const displayMove = () => {
-        // once you get a move from the gameControl -> 
-        // gameBoard -> display
-        // display it 
-    }
-    return {};
+    return {
+        displayMove,
+        resetBoard};
+
 })();
 
-
 const gameControl = (() => {
+    const player0 = player()
+    const player1 = player()
     let playerCounter = 0
 
     const turnControl = () => {
-        playerCounter ++
-        if (playerCounter ===1){
-            return "O"
-        } else if (playerCounter === 2){
-            playerCounter = 0
-            return "X"
+        if (playerCounter === 0) {
+            playerCounter++
+            return 0
+        } else if (playerCounter === 1) {
+            playerCounter--
+            return 1
         }
     };
 
-    const checkLegalMove = function (cellNumber) {
-        return (gameBoard.getLegalMoves().includes(cellNumber) ? true : false) 
-    };
+    const winningSets = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
 
+    const getPlayer = (playerIndex) => {
+        return (playerIndex === 0 ? player0: player1)
+    }
+
+    const logMoves = (playerIndex, cell) =>{
+        return getPlayer(playerIndex).registerMove(cell)
+    }
+
+    const checkLegalMove = (cellNumber) => {
+        return (gameBoard.getLegalMoves().includes(cellNumber) ? true : false)
+    };
 
     const registerMove = function () {
         const cellNumber = parseInt(this.id.slice(4))
         if (checkLegalMove(cellNumber)) {
-            const sign = turnControl()
-            gameBoard.setMove(cellNumber, sign)
+            const playerIndex = turnControl()
+            gameBoard.setMove(cellNumber, playerIndex)
+            logMoves(playerIndex, cellNumber)
+            displayControl.displayMove(playerIndex, cellNumber)
+            checkWin(playerIndex)
         }
     }
 
@@ -87,8 +137,22 @@ const gameControl = (() => {
         }
     }
 
-    registerClick()
+    const gameEnd = () => {
+    }
 
+    const checkWin = (playerIndex) => {
+        const player = getPlayer(playerIndex)
+        for (let i = 0; i < winningSets.length; i++){
+            const winCondition = winningSets[i]
+            if (winCondition.every(v => player.movesMade.includes(v))){
+                console.log("WIN!!!!")
+                return true
+            }
+        }
+        return false 
+    }
+
+    registerClick()
 })();
 
 
